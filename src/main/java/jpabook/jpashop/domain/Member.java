34227c5b0e020a9caa4018357aeffcbda3e3722f
jhpark0912 <jhprk0912@fasoo.com>
2023-01-26 14:24:51 +0900

    0126 성능 최적화 API개발 고급

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,9 +19,11 @@ public class Member {
     @NotEmpty
     private String name;
 
+    @JsonIgnore
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //order table의 member에 매핑되었다는 뜻
     private List<Order> orders = new ArrayList<>();
 }
